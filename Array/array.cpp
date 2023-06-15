373cef84eb88d68eb934ed94e71a62a301d7f45f
arya4868 <soravarya175@gmail.com>
2023-06-15 18:30:41 +0530

    12-15 june 2023

---
+++ Array/array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void printarray(int arr[], int size){
+    for(int i =0; i< size; i++){
+        cout << arr[i] ;
+    }
+    cout << " Printing done" << endl;
+}
+
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    cout << " The value at index 1 is " << arr[1] << endl; // by default garbage value will be present.
+
+    printarray(arr, 5);
+}
